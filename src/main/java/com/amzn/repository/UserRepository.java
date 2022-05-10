e880b4257b2c6083de075cdee2468805df0199f5
Shad95 <shadk1952@gmail.com>
2022-05-10 14:55:42 +0530

    initial commit

---
+++ src/main/java/com/amzn/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.amzn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.amzn.entity.User;
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+
+}
---
+++ src/test/java/com/amzn/AwsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amzn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
